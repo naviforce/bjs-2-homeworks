0a94a7c52da62f37d682d91afef11aa42ac9b7a7
naviforce <dns15@mail.ru>
2023-06-05 18:35:28 +0200

    Третье задание готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
+"use strict"
+
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);  // boolean
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, arr) =>  {
+        acc += item;
+        if(index === arr.length - 1)    {
+            return acc / arr.length;
+        }
+        return acc;
+    }, 0);
+    return result; // array
 }

1f46ec409b7f24a5c2afa94cb41ee056f75d09d3
naviforce <dns15@mail.ru>
2023-06-08 22:32:56 +0200

    Шестое задание готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,59 @@
-﻿
+﻿"use strict"
+
+function parseCount(num)    {
+    let parseNum = Number.parseFloat(num);
+    if (Number.isNaN(parseNum)) {
+        const error = new Error("Невалидное значение");
+        throw error; 
+    }
+    return parseNum;
+}
+
+function validateCount(num)    {
+    try {
+      return parseCount(num);
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+}
+
+
+//калькулятор треугольников
+
+
+class Triangle {
+    constructor (a, b, c)   {
+      if ((a + b) < c || (a + c) < b || (b + c) < a) {
+        throw new Error("Треугольник с такими сторонами не существует");
+      }
+      this.a = a;
+      this.b = b;
+      this.c = c;
+    }
+    
+    get perimeter()  {
+        return this.a + this.b + this.c;
+    }
+
+    get area()  {
+        let p = 0.5 * this.perimeter;
+        return parseFloat((Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c)   {
+    try {
+        return new Triangle(a, b, c);
+    }   catch (error) {
+        return  {
+            get perimeter() {
+                return "Ошибка! Треугольник не существует";
+            },
+            
+            get area() {
+                return "Ошибка! Треугольник не существует";
+            }
+        }
+    }
+}
